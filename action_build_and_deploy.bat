29f2802958aaf7960ba9494197e216e37adfde71
Oom Lout <aaron@oomlout.com>
2024-10-12 19:28:13 +0100

    Build and deploy

--- action_build_and_deploy.bat
+++ action_build_and_deploy.bat
@@ -1,5 +1,5 @@
 z:
-cd oomlout_oomp_current_version_messy
+cd oomlout_oomp_current_version_fast_test
 python action_build_oomp.py
 git add *
 git commit -m "Build and deploy"
---
+++ navigation/readme.md
@@ -0,0 +1,3 @@
+* [camera](camera)
+  * [cctv_dome](camera/cctv_dome)
+    * [140_mm](camera/cctv_dome/140_mm)
