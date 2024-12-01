35e5c0b28655d9531a7645881fc273503d9e33ec
Oom Lout <aaron@oomlout.com>
2024-12-01 11:10:03 +0000

    new structure

--- action_build_oomp_time_analysis_wrapper.py
+++ action_build_oomp_time_analysis_wrapper.py
@@ -15,11 +15,12 @@ def main(**kwargs):
     action_build_oomp.main(**kwargs)  # Replace with the actual function call
     
     profiler.disable()
-    profiler.dump_stats('profile_output.prof')
-    print("Profile data saved to profile_output.prof. Use 'snakeviz profile_output.prof' to visualize.")
+    stat_file = 'temporary/profile_output.prof'
+    profiler.dump_stats(f'{stat_file}')
+    print(f"Profiling data saved to {stat_file}")
 
     #launch visualizer
-    os.system("snakeviz profile_output.prof")
+    os.system(f'python -m pstats {stat_file}')
  
 
 
